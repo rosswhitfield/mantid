efea73ce74bf85cfe5d845ef32e0169c56695cd2
Ross Whitfield <whitfieldre1@ornl.gov>
2025-12-04 12:54:49 +1100

    Fix PropertyWithValue::g_logger

--- Framework/DataObjects/src/PropertyWithValue.cpp
+++ Framework/DataObjects/src/PropertyWithValue.cpp
@@ -32,6 +32,41 @@ template <size_t nd> using MDLeanEventWS = MDEventWorkspace<MDLeanEvent<nd>, nd>
 } // namespace DataObjects
 namespace Kernel {
 
+// Explicit static logger instantiations
+#define PROPERTYWITHVALUE_LOGGER(type) template <> Logger PropertyWithValue<type>::g_logger("PropertyWithValue");
+
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::EventWorkspace>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::GroupingWorkspace>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MaskWorkspace>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDEventWS<1>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDEventWS<2>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDEventWS<3>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDEventWS<4>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDEventWS<5>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDEventWS<6>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDEventWS<7>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDEventWS<8>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDEventWS<9>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDLeanEventWS<1>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDLeanEventWS<2>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDLeanEventWS<3>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDLeanEventWS<4>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDLeanEventWS<5>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDLeanEventWS<6>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDLeanEventWS<7>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDLeanEventWS<8>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDLeanEventWS<9>>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::MDHistoWorkspace>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::OffsetsWorkspace>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::PeaksWorkspace>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::LeanElasticPeaksWorkspace>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::RebinnedOutput>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::SpecialWorkspace2D>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::SplittersWorkspace>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::TableWorkspace>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::Workspace2D>)
+PROPERTYWITHVALUE_LOGGER(std::shared_ptr<DataObjects::WorkspaceSingleValue>)
+
 /// @cond
 template class MANTID_DATAOBJECTS_DLL PropertyWithValue<std::shared_ptr<DataObjects::EventWorkspace>>;
 template class MANTID_DATAOBJECTS_DLL PropertyWithValue<std::shared_ptr<DataObjects::GroupingWorkspace>>;
