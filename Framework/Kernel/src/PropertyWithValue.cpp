47c9d84b1924ceedd12de0e0cb536d819032dcc4
Ross Whitfield <whitfieldre1@ornl.gov>
2025-12-04 15:29:06 +1100

    Revert "del"
    This reverts commit 29604af26f6bb69d17243346b4629b2de4dc29f1.

--- Framework/Kernel/src/PropertyWithValue.cpp
+++ Framework/Kernel/src/PropertyWithValue.cpp
@@ -75,6 +75,12 @@ PROPERTYWITHVALUE_LOGGER(std::vector<long>)
 PROPERTYWITHVALUE_LOGGER(std::vector<unsigned long>)
 PROPERTYWITHVALUE_LOGGER(std::vector<std::vector<long>>)
 PROPERTYWITHVALUE_LOGGER(std::vector<std::vector<unsigned long>>)
+PROPERTYWITHVALUE_LOGGER(long long)
+PROPERTYWITHVALUE_LOGGER(unsigned long long)
+PROPERTYWITHVALUE_LOGGER(std::vector<long long>)
+PROPERTYWITHVALUE_LOGGER(std::vector<unsigned long long>)
+PROPERTYWITHVALUE_LOGGER(std::vector<std::vector<long long>>)
+PROPERTYWITHVALUE_LOGGER(std::vector<std::vector<unsigned long long>>)
 #endif
 #ifdef __linux__
 PROPERTYWITHVALUE_LOGGER(long long)
