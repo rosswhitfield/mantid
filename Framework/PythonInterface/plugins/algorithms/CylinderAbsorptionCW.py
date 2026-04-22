a9581de26054265ad6c9eaa2a11cfd5ed9a9e0e1
Ross Whitfield <whitfieldre1@ornl.gov>
2026-04-22 11:39:39 +1000

    Add tests

--- Framework/PythonInterface/plugins/algorithms/CylinderAbsorptionCW.py
+++ Framework/PythonInterface/plugins/algorithms/CylinderAbsorptionCW.py
@@ -9,7 +9,7 @@ import numpy as np
 from scipy.special import modstruve, i0, i1
 
 from mantid.api import AlgorithmFactory, PythonAlgorithm, WorkspaceProperty
-from mantid.kernel import Direction, Property, StringListValidator
+from mantid.kernel import Direction, Property, StringListValidator, FloatBoundedValidator
 from mantid.simpleapi import CreateWorkspace, CreateSingleValuedWorkspace
 from mantid.geometry import GeometryShape
 
@@ -23,9 +23,9 @@ def bilinear_interpolate(x, y, x_grid, y_grid, Z):
     # Find bracketing indices
     ix = np.searchsorted(x_grid, x) - 1
     iy = np.searchsorted(y_grid, y) - 1
-    # Clamp to valid cell
-    ix = max(0, min(ix, len(x_grid) - 2))
-    iy = max(0, min(iy, len(y_grid) - 2))
+    # raise if out of bounds
+    if ix < 0 or ix >= len(x_grid) - 1 or iy < 0 or iy >= len(y_grid) - 1:
+        raise ValueError(f"Interpolation point (x={x}, y={y}) is out of bounds of the grid.")
 
     x1, x2 = x_grid[ix], x_grid[ix + 1]
     y1, y2 = y_grid[iy], y_grid[iy + 1]
@@ -43,6 +43,203 @@ def bilinear_interpolate(x, y, x_grid, y_grid, Z):
 
     # Bilinear blend
     return (1 - tx) * (1 - ty) * Q11 + tx * (1 - ty) * Q21 + (1 - tx) * ty * Q12 + tx * ty * Q22
+
+
+# This is table 1 from Blech and Averbach 1964
+δ_grid = np.array(
+    [  # μR = 0
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # μR = 0.1
+        [
+            0.1049,
+            0.1023,
+            0.1001,
+            0.0981,
+            0.0963,
+            0.0946,
+            0.0931,
+            0.0916,
+            0.0902,
+            0.0888,
+            0.0895,
+            0.0819,
+            0.0768,
+            0.0615,
+            0.0425,
+            0.0333,
+            0.0277,
+            0.0237,
+        ],
+        # μR = 0.2
+        [
+            0.1922,
+            0.1878,
+            0.1841,
+            0.1809,
+            0.178,
+            0.1752,
+            0.1726,
+            0.1701,
+            0.1677,
+            0.1654,
+            0.1631,
+            0.1536,
+            0.1445,
+            0.1174,
+            0.0818,
+            0.0644,
+            0.0535,
+            0.046,
+        ],
+        # μR = 0.3
+        [
+            0.2657,
+            0.26,
+            0.2553,
+            0.2512,
+            0.2475,
+            0.244,
+            0.2407,
+            0.2376,
+            0.2345,
+            0.2316,
+            0.2287,
+            0.2165,
+            0.2044,
+            0.1682,
+            0.1181,
+            0.0933,
+            0.0778,
+            0.067,
+        ],
+        # μR = 0.4
+        [
+            0.3286,
+            0.3212,
+            0.3157,
+            0.311,
+            0.3067,
+            0.3028,
+            0.2991,
+            0.2955,
+            0.2921,
+            0.2887,
+            0.2854,
+            0.2715,
+            0.2573,
+            0.2143,
+            0.1516,
+            0.1203,
+            0.1005,
+            0.0867,
+        ],
+        # μR = 0.5
+        [
+            None,
+            0.3742,
+            0.367,
+            0.3616,
+            0.357,
+            0.3527,
+            0.3488,
+            0.345,
+            0.3413,
+            0.3377,
+            0.3342,
+            0.3193,
+            0.3036,
+            0.256,
+            0.1825,
+            0.1454,
+            0.1218,
+            0.1053,
+        ],
+        # μR = 0.6
+        [
+            None,
+            None,
+            0.4118,
+            0.4046,
+            0.3933,
+            0.3947,
+            0.3905,
+            0.3866,
+            0.3829,
+            0.3792,
+            0.3756,
+            0.3605,
+            0.3439,
+            0.2937,
+            0.211,
+            0.1687,
+            0.1418,
+            0.1227,
+        ],
+        # μR = 0.7
+        [
+            None,
+            None,
+            None,
+            0.4422,
+            0.4349,
+            0.4295,
+            0.425,
+            0.421,
+            0.4172,
+            0.4136,
+            0.41,
+            0.3953,
+            0.3785,
+            0.3273,
+            0.237,
+            0.1904,
+            0.1604,
+            0.1391,
+        ],
+        # μR = 0.8
+        [
+            None,
+            None,
+            None,
+            None,
+            0.4661,
+            0.4585,
+            0.453,
+            0.4486,
+            0.4447,
+            0.4411,
+            0.4376,
+            0.4239,
+            0.4073,
+            0.3571,
+            0.2607,
+            0.2103,
+            0.1776,
+            0.1543,
+        ],
+        # μR = 0.9
+        [
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            0.4751,
+            0.4701,
+            0.4657,
+            0.4619,
+            0.4586,
+            0.4461,
+            0.4303,
+            0.383,
+            0.282,
+            0.2286,
+            0.1936,
+            0.1685,
+        ],
+    ]
+)
 
 
 class CylinderAbsorptionCW(PythonAlgorithm):
@@ -63,7 +260,7 @@ class CylinderAbsorptionCW(PythonAlgorithm):
             WorkspaceProperty("InputWorkspace", "", direction=Direction.Input),
             "Input workspace",
         )
-        self.declareProperty("Wavelength", 1.7982, doc="Wavelength in Angstroms.")
+        self.declareProperty("Wavelength", Property.EMPTY_DBL, FloatBoundedValidator(lower=0.0), doc="Wavelength in Angstroms.")
         self.declareProperty(
             "Radius",
             Property.EMPTY_DBL,
@@ -78,7 +275,7 @@ class CylinderAbsorptionCW(PythonAlgorithm):
         self.declareProperty(
             "AttenuationXSection",
             Property.EMPTY_DBL,
-            doc="Attenuation cross-section in barn at 1.798 Å. If not provided, it will be inferred from the workspace sample material.",
+            doc="Attenuation cross-section in barn at 1.7982 Å. If not provided, it will be inferred from the workspace sample material.",
         )
         self.declareProperty(
             "ScatteringXSection",
@@ -147,6 +344,14 @@ class CylinderAbsorptionCW(PythonAlgorithm):
                 issues["AttenuationXSection"] = msg
                 issues["ScatteringXSection"] = msg
                 issues["SampleNumberDensity"] = msg
+        elif (
+            self.getProperty("AttenuationXSection").isDefault
+            or self.getProperty("ScatteringXSection").isDefault
+            or self.getProperty("SampleNumberDensity").isDefault
+        ):
+            issues["AttenuationXSection"] = "Attenuation and scattering cross-section properties must be provided together."
+            issues["ScatteringXSection"] = "Attenuation and scattering cross-section properties must be provided together."
+            issues["SampleNumberDensity"] = "Attenuation and scattering cross-section properties must be provided together."
 
         return issues
 
@@ -225,201 +430,6 @@ class CylinderAbsorptionCW(PythonAlgorithm):
                 [0.10, 0.12, 0.14, 0.16, 0.18, 0.20, 0.22, 0.24, 0.26, 0.28, 0.30, 0.40, 0.50, 1.00, 2.00, 3.00, 4.00, 5.00]
             )
             μR_grid = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-
-            δ_grid = np.array(
-                [  # μR = 0
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    # μR = 0.1
-                    [
-                        0.1049,
-                        0.1023,
-                        0.1001,
-                        0.0981,
-                        0.0963,
-                        0.0946,
-                        0.0931,
-                        0.0916,
-                        0.0902,
-                        0.0888,
-                        0.0895,
-                        0.0819,
-                        0.0768,
-                        0.0615,
-                        0.0425,
-                        0.0333,
-                        0.0277,
-                        0.0237,
-                    ],
-                    # μR = 0.2
-                    [
-                        0.1922,
-                        0.1878,
-                        0.1841,
-                        0.1809,
-                        0.178,
-                        0.1752,
-                        0.1726,
-                        0.1701,
-                        0.1677,
-                        0.1654,
-                        0.1631,
-                        0.1536,
-                        0.1445,
-                        0.1174,
-                        0.0818,
-                        0.0644,
-                        0.0535,
-                        0.046,
-                    ],
-                    # μR = 0.3
-                    [
-                        0.2657,
-                        0.26,
-                        0.2553,
-                        0.2512,
-                        0.2475,
-                        0.244,
-                        0.2407,
-                        0.2376,
-                        0.2345,
-                        0.2316,
-                        0.2287,
-                        0.2165,
-                        0.2044,
-                        0.1682,
-                        0.1181,
-                        0.0933,
-                        0.0778,
-                        0.067,
-                    ],
-                    # μR = 0.4
-                    [
-                        0.3286,
-                        0.3212,
-                        0.3157,
-                        0.311,
-                        0.3067,
-                        0.3028,
-                        0.2991,
-                        0.2955,
-                        0.2921,
-                        0.2887,
-                        0.2854,
-                        0.2715,
-                        0.2573,
-                        0.2143,
-                        0.1516,
-                        0.1203,
-                        0.1005,
-                        0.0867,
-                    ],
-                    # μR = 0.5
-                    [
-                        None,
-                        0.3742,
-                        0.367,
-                        0.3616,
-                        0.357,
-                        0.3527,
-                        0.3488,
-                        0.345,
-                        0.3413,
-                        0.3377,
-                        0.3342,
-                        0.3193,
-                        0.3036,
-                        0.256,
-                        0.1825,
-                        0.1454,
-                        0.1218,
-                        0.1053,
-                    ],
-                    # μR = 0.6
-                    [
-                        None,
-                        None,
-                        0.4118,
-                        0.4046,
-                        0.3933,
-                        0.3947,
-                        0.3905,
-                        0.3866,
-                        0.3829,
-                        0.3792,
-                        0.3756,
-                        0.3605,
-                        0.3439,
-                        0.2937,
-                        0.211,
-                        0.1687,
-                        0.1418,
-                        0.1227,
-                    ],
-                    # μR = 0.7
-                    [
-                        None,
-                        None,
-                        None,
-                        0.4422,
-                        0.4349,
-                        0.4295,
-                        0.425,
-                        0.421,
-                        0.4172,
-                        0.4136,
-                        0.41,
-                        0.3953,
-                        0.3785,
-                        0.3273,
-                        0.237,
-                        0.1904,
-                        0.1604,
-                        0.1391,
-                    ],
-                    # μR = 0.8
-                    [
-                        None,
-                        None,
-                        None,
-                        None,
-                        0.4661,
-                        0.4585,
-                        0.453,
-                        0.4486,
-                        0.4447,
-                        0.4411,
-                        0.4376,
-                        0.4239,
-                        0.4073,
-                        0.3571,
-                        0.2607,
-                        0.2103,
-                        0.1776,
-                        0.1543,
-                    ],
-                    # μR = 0.9
-                    [
-                        None,
-                        None,
-                        None,
-                        None,
-                        None,
-                        None,
-                        0.4751,
-                        0.4701,
-                        0.4657,
-                        0.4619,
-                        0.4586,
-                        0.4461,
-                        0.4303,
-                        0.383,
-                        0.282,
-                        0.2286,
-                        0.1936,
-                        0.1685,
-                    ],
-                ]
-            )
 
             R_over_h = radius / height
 
